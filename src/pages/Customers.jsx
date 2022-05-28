e0cc92b0b26ddf519c3a81bed416df33a18160d8
KRITSAKORN CHOATCHAOJARU <amajakai14@gmail.com>
2022-05-28 13:33:56 +0900

    first commit

---
+++ src/pages/Customers.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  Search,
+  ColumnDirective,
+  Page,
+  Selection,
+  Inject,
+  Edit,
+  Toolbar,
+  Sort,
+  Filter,
+} from "@syncfusion/ej2-react-grids";
+
+import { customersData, customersGrid } from "./../data/dummy";
+import { Header } from "../components";
+const Customers = () => {
+  const ToolbarOptions = ["Search", "Delete"];
+  const editing = { allowDeleting: true, allowEditing: true };
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={customersData}
+        allowPagingData
+        allowSorting
+        toolbar={ToolbarOptions}
+        width="auto"
+        editSettings={editing}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Page, Search, Toolbar, Selection, Edit, Filter, Sort]}
+        />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Customers;
